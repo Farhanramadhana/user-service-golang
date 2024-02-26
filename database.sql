0e5a1208bae4c78da783e905e64084340377a382
Farhan Ramadhana <farhanramadhana97@gmail.com>
2024-02-26 14:38:28 +0700

    update sql

--- database.sql
+++ database.sql
@@ -1,8 +1,8 @@
-CREATE TABLE user (
+CREATE TABLE users (
 	id serial PRIMARY KEY,
   full_name VARCHAR(60) NOT NULL,
-  phone_number VARCHAR(13) NOT NULL,
+  phone_number VARCHAR(13) NOT NULL UNIQUE,
   password VARCHAR(60) NOT NULL,
-  created_at timestampttz,
-  updated_at timestampttz
+  created_at timestamptz,
+  updated_at timestamptz
 );
