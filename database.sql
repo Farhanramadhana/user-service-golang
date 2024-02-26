850e5ce46edac04b5c9053fa6c188bd929dc416d
Farhan Ramadhana <farhanramadhana97@gmail.com>
2024-02-26 21:51:08 +0700

    add login_logs table

--- database.sql
+++ database.sql
@@ -6,3 +6,12 @@ CREATE TABLE users (
   created_at timestamptz,
   updated_at timestamptz
 );
+
+CREATE TABLE login_logs(
+  id serial PRIMARY KEY,
+  user_id int REFERENCES users(id),
+  total_login_success int,
+  last_login_at timestamptz,
+  created_at timestamptz,
+  updated_at timestamptz
+);
