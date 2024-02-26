a4098a14ff65337d92311079d5b390b8f66f210e
Farhan Ramadhana <farhanramadhana97@gmail.com>
2024-02-26 14:39:17 +0700

    finishing create user

--- handler/endpoints.go
+++ handler/endpoints.go
@@ -1,12 +1,16 @@
 package handler
 
 import (
+	"context"
 	"net/http"
+	"time"
 	"userService/model"
 	"userService/model/constant"
+	"userService/repository"
 
 	validator "github.com/go-playground/validator/v10"
 	"github.com/labstack/echo/v4"
+	"golang.org/x/crypto/bcrypt"
 )
 
 func (s *Server) Hello(ctx echo.Context) error {
@@ -34,6 +38,34 @@ func (s *Server) CreateUser(ctx echo.Context) error {
 			Status:           constant.ERROR,
 			ValidationErrors: errMessageList,
 		})
+	}
+
+	hash, err := bcrypt.GenerateFromPassword([]byte(request.Password), bcrypt.MinCost)
+	if err != nil {
+		return err
+	}
+
+	// get user by phone number, if exist return error
+	existingUser, _ := s.Repository.GetUserByPhone(context.TODO(), request.PhoneNumber)
+	if existingUser.Id != 0 {
+		return ctx.JSON(400, model.ResponseError{
+			Status:  constant.ERROR,
+			Message: "phone number already exist",
+		})
+	}
+
+	// register user, phone number is unique
+	newUser := repository.UserTable{
+		FullName:    request.FullName,
+		PhoneNumber: request.PhoneNumber,
+		Password:    string(hash),
+		CreatedAt:   time.Now(),
+		UpdatedAt:   time.Now(),
+	}
+
+	err = s.Repository.CreateUser(context.TODO(), newUser)
+	if err != nil {
+		return err
 	}
 
 	return ctx.JSON(201, request)
--- model/request.go
+++ model/request.go
@@ -3,5 +3,5 @@ package model
 type RequestCreateUser struct {
 	FullName    string `json:"full_name" validate:"min=3,max=60"`
 	PhoneNumber string `json:"phone_number" validate:"min=10,max=13,startswith=+62"`
-	Password    string `json:"password" validate:"min=6,max=64,alphanum,hasupper,hasnumber,hasspecial"`
+	Password    string `json:"password" validate:"min=6,max=64,hasupper,hasnumber,hasspecial"`
 }
